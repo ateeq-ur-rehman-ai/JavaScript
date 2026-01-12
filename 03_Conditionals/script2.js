49ce6021ff26c2ab6b08c175a4a23deced60c982
Ateeq Ur Rehman <ateeqkhalid623@gmail.com>
2026-01-12 15:58:48 +0500

    Add files via upload

---
+++ 03_Conditionals/script2.js
@@ -0,0 +1,14 @@
+// Grade Calculation
+let marks =89;
+if (marks>90){
+    console.log("A+ Grade");
+}
+else if (marks>80){
+    console.log("A Grade");
+}   
+else if (marks>70){
+    console.log("B Grade");
+}
+else{
+    console.log("C Grade");  
+}
