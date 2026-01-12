47244bf38e8ac2d34ba4aa7e98e2be3eca667d8f
Ateeq Ur Rehman <ateeqkhalid623@gmail.com>
2026-01-12 14:42:29 +0500

    Add files via upload

---
+++ 02_Variables DataTypes And Objects/script.js
@@ -0,0 +1,34 @@
+var a =20;
+var b=78;
+var c ="Hello World";
+
+console.log(a);
+console.log(b);
+console.log(c);
+
+// var 55a = "Rohan"; // Not Allowed
+
+console.log(a +  b + 8);
+console.log(typeof a, typeof b, typeof c);
+
+let x = "Hello";
+let y = 22;
+let z = 3.55;
+const p = true;
+let q = undefined;
+let r = null;
+
+console.log(x, y, z, p, q, r)
+console.log(typeof x, typeof y, typeof z, typeof p, typeof q, typeof r)
+
+let data = {
+    "name": "jack",
+    "job code": 59820,
+    "is_handsome": true
+}
+
+console.log(data);
+data.salary = "10k";
+console.log(data);
+data.salary = "20k";
+console.log(data);
