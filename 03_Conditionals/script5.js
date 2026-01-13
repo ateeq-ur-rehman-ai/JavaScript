0abf28541e7c65af865ac384ece602d1c945de4d
Ateeq Ur Rehman <ateeqkhalid623@gmail.com>
2026-01-13 23:28:02 +0500

    Add files via upload

---
+++ 03_Conditionals/script5.js
@@ -0,0 +1,22 @@
+// Day of the week using switch case
+let day = 4;
+
+switch (day) {
+    case 1:
+        console.log("Monday");
+        break;
+    case 2:
+        console.log("Tuesday");
+        break;
+    case 3:
+        console.log("Wednesday");
+        break;
+    case 4:
+        console.log("Thursday");    
+        break;
+    case 5:
+        console.log("Friday");
+        break;
+    default:
+        console.log("Invalid day");
+}
