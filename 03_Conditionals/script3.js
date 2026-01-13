3ebb71bc4b4168aae3010fc8958027802e29ecfb
Ateeq Ur Rehman <ateeqkhalid623@gmail.com>
2026-01-13 23:27:38 +0500

    Add files via upload

---
+++ 03_Conditionals/script3.js
@@ -0,0 +1,13 @@
+// Nested If-Else Example
+let user=false;
+let companyuser=true;
+if (user){
+    if (companyuser){
+        console.log("Welcome Employee");
+    }
+    else{
+        console.log("Welcome User");
+    }   
+}else{
+    console.log("Welcome Guest");
+}
