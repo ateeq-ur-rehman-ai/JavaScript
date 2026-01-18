47129db5a2006d254c7af1db66613d6b32a61908
Ateeq Ur Rehman <ateeqkhalid623@gmail.com>
2026-01-18 15:54:53 +0500

    Add files via upload

---
+++ 07_Strings/script.js
@@ -0,0 +1,13 @@
+console.log("We are learning strings.");
+let n="Javascript";
+console.log(n[0]);
+console.log(n[1]);
+console.log(n[2]);
+console.log(n[3]);
+console.log(n[4]);
+console.log(n[5]);
+console.log(n[6]);
+console.log(n[7]);
+console.log(n[8]);
+console.log(n[9]);
+console.log(n.length);
