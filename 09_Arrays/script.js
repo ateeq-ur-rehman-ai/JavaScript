aefce83339893903efd28e966de6ffa8a0e50266
Ateeq Ur Rehman <ateeqkhalid623@gmail.com>
2026-01-21 22:44:22 +0500

    Add files via upload

---
+++ 09_Arrays/script.js
@@ -0,0 +1,14 @@
+let arr = [1,3,5,2,4,8,3,7];
+console.log(arr);
+console.log(arr.length);
+console.log(arr[0]);
+
+
+console.log(arr.toString())
+console.log(arr.join(" and "))
+
+let numbers = [1, 2, 3, 4, 5] 
+numbers.splice(1, 2)    
+numbers.splice(1, 3)  
+numbers.splice(1, 3, 222, 333) 
+console.log(numbers)
